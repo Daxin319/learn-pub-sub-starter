7f9636d516d138475c99f7a60a5cf430d28c4ba7
Lyle Higginbotham <lylehigg@gmail.com>
2026-03-11 01:48:53 -0600

    finished first two chapters of the course plus the first lesson in chapter 3

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	dial, err := amqp.Dial(connectionString)
+	if err != nil {
+		return
+	}
+	defer dial.Close()
+
+	fmt.Println("Connection Successful!")
+
+	newChannel, err := dial.Channel()
+	if err != nil {
+		return
+	}
+	defer newChannel.Close()
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("\nExiting...")
+
+	pubsub.PublishJSON(newChannel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
 }
